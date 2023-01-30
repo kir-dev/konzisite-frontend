690fce130438cdbd26f6bbd74b14bb00b8f1ee76
Sámuel Fekete <feketesamu@gmail.com>
2023-01-30 12:03:39 +0100

    remove cookie consent (#62)

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './api/contexts/auth/AuthContext'
-import { CookieConsentProvider } from './api/contexts/cookie-consent/CookieConsentContext'
 import { App } from './App'
 import customTheme from './assets/theme'
 import { initAxios, queryClient } from './util/query-client'
@@ -18,16 +17,14 @@ root.render(
   <React.StrictMode>
     <ColorModeScript />
     <ChakraProvider theme={customTheme}>
-      <CookieConsentProvider>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <AuthProvider>
-              <App />
-              <ReactQueryDevtools />
-            </AuthProvider>
-          </BrowserRouter>
-        </QueryClientProvider>
-      </CookieConsentProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AuthProvider>
+            <App />
+            <ReactQueryDevtools />
+          </AuthProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 )
