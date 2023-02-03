bf286b0d6fc5679a8b48bc86cf24d1a8c1c64445
Sámuel Fekete <feketesamu@gmail.com>
2023-02-03 16:03:08 +0100

    conflict fix and #82

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -30,47 +30,47 @@ export const ErrorPage = ({ title, messages, backPath, status }: ErrorPageState)
 
   useEffect(() => {
     if (status === 401 || s === 401) {
-      toast({ title: '401 Nem vagy bejelentkezve', status: 'error' })
+      toast({ title: 'Nem vagy bejelentkezve', status: 'error' })
       onLogout('/login')
     }
   }, [status, s])
 
   return status === 401 || s === 401 ? null : (
-      <>
+    <>
       <Helmet title="Hiba" />
-    <Alert p={10} status="error" variant="subtle" flexDirection="column" alignItems="center" justifyContent="center" textAlign="center">
-      <AlertIcon boxSize="40px" mr={0} />
-      <AlertTitle mt={4} mb={3} fontSize="2xl">
-        {title || t || 'Error occured'}
-      </AlertTitle>
-      {status && <Image maxHeight={400} src={`/img/${status}.jpg`} />}
-      <AlertDescription>
-        <VStack justifyContent="center" spacing={1}>
-          {messages?.filter(Boolean).map((errorMsg) => (
-            <span key={errorMsg}>{errorMsg}</span>
-          ))}
-          {m && (
-            <>
-              {m?.filter(Boolean).map((errorMsg) => (
-                <span key={errorMsg}>{errorMsg}</span>
-              ))}
-            </>
-          )}
-        </VStack>
-      </AlertDescription>
-      <Button
-        colorScheme="brand"
-        mt={6}
-        leftIcon={<FaChevronLeft />}
-        onClick={() => {
-          if (backPath) navigate(backPath)
-          else if (bp) navigate(bp)
-          else navigate(-1)
-        }}
-      >
-        Vissza
-      </Button>
-    </Alert>
+      <Alert p={10} status="error" variant="subtle" flexDirection="column" alignItems="center" justifyContent="center" textAlign="center">
+        <AlertIcon boxSize="40px" mr={0} />
+        <AlertTitle mt={4} mb={3} fontSize="2xl">
+          {title || t || 'Error occured'}
+        </AlertTitle>
+        {status && <Image maxHeight={400} src={`/img/${status}.jpg`} />}
+        <AlertDescription>
+          <VStack justifyContent="center" spacing={1}>
+            {messages?.filter(Boolean).map((errorMsg) => (
+              <span key={errorMsg}>{errorMsg}</span>
+            ))}
+            {m && (
+              <>
+                {m?.filter(Boolean).map((errorMsg) => (
+                  <span key={errorMsg}>{errorMsg}</span>
+                ))}
+              </>
+            )}
+          </VStack>
+        </AlertDescription>
+        <Button
+          colorScheme="brand"
+          mt={6}
+          leftIcon={<FaChevronLeft />}
+          onClick={() => {
+            if (backPath) navigate(backPath)
+            else if (bp) navigate(bp)
+            else navigate(-1)
+          }}
+        >
+          Vissza
+        </Button>
+      </Alert>
     </>
   )
 }
