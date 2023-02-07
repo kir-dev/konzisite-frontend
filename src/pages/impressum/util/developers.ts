2625470f047407f8a3ea11970af6baddf0aed5e8
Száraz Dániel <65909866+DannySS123@users.noreply.github.com>
2023-02-07 13:45:40 +0100

    impressum (#99)

---
+++ src/pages/impressum/util/developers.ts
@@ -0,0 +1,44 @@
+export type Dev = { name: string; img: string; tags: string[] }
+
+export const developers: Dev[] = [
+  {
+    name: 'Samu',
+    img: 'https://warp.sch.bme.hu/images/feketesamu1.jpg',
+    tags: ['Project Leader', 'Backend', 'Frontend']
+  },
+  {
+    name: 'Dani',
+    img: 'https://warp.sch.bme.hu/images/szaraz_daniel1',
+    tags: ['Backend', 'Frontend']
+  },
+  {
+    name: 'Kolos',
+    img: 'https://warp.sch.bme.hu/images/kisbogdan',
+    tags: ['Backend', 'Frontend']
+  },
+  {
+    name: 'Tomi',
+    img: 'https://warp.sch.bme.hu/images/tomitheninja',
+    tags: ['Backend']
+  },
+  {
+    name: 'Smuky',
+    img: 'https://warp.sch.bme.hu/images/smukferenc',
+    tags: ['Backend']
+  },
+  {
+    name: 'Máté',
+    img: 'https://warp.sch.bme.hu/images/nmate',
+    tags: ['Backend']
+  },
+  {
+    name: 'Tamás',
+    img: '',
+    tags: ['Backend', 'Frontend']
+  },
+  {
+    name: 'Trisz',
+    img: 'https://warp.sch.bme.hu/images/triszt4n1',
+    tags: ['Backend', 'Frontend']
+  }
+]
