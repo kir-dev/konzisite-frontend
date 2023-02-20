02b8cd277e8d075b703c908361497ff2c799ab58
Sámuel Fekete <feketesamu@gmail.com>
2023-02-20 18:49:41 +0100

    plausible (#115)

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import Plausible from 'plausible-tracker'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
@@ -8,11 +9,18 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './api/contexts/auth/AuthContext'
 import { App } from './App'
 import customTheme from './assets/theme'
+import { API_HOST } from './util/environment'
 import { initAxios, queryClient } from './util/query-client'
 
 initAxios()
 
 const root = createRoot(document.getElementById('root')!)
+
+const { enableAutoPageviews } = Plausible({
+  domain: API_HOST,
+  apiHost: 'https://visit.kir-dev.hu'
+})
+enableAutoPageviews()
 
 root.render(
   <React.StrictMode>
