1d9f9d581e523dcd0a9e70360ca19b84157158bd
Sámuel Fekete <feketesamu@gmail.com>
2023-03-04 21:56:44 +0100

    remove version from footer
    doesn't really work in prod

--- src/util/environment.ts
+++ src/util/environment.ts
@@ -1,3 +1,2 @@
 export const API_HOST = process.env.REACT_APP_API_HOST || 'http://localhost:3300'
-export const APP_VERSION = process.env.REACT_APP_VERSION || '1.0.0'
 export const OLD_KONZISITE = process.env.REACT_APP_OLD_KONZISITE || 'https://konzi.vik.hk'
