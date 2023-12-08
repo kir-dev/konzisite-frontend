eaf02a99ecec1ef8d5b1f3b5066bc9a5df0f9ce5
Sámuel Fekete <feketesamu@gmail.com>
2023-12-08 18:00:57 +0100

    more improvements

--- src/pages/user/components/UserStatCard.tsx
+++ src/pages/user/components/UserStatCard.tsx
@@ -9,17 +9,17 @@ export const UserStatCard = ({ stats }: { stats?: UserStats }) => {
     {
       value: stats?.presentationCount,
       label: t('profilePage.presLabel', { count: stats?.presentationCount }),
-      explanation: t('profilePage.presExpl', { data: stats?.presentationCount })
+      explanation: t('profilePage.presExpl', { count: stats?.presentationCount })
     },
     {
       value: stats?.allParticipants,
       label: t('profilePage.allParLabel', { count: stats?.allParticipants }),
-      explanation: t('profilePage.allParExpl', { data: stats?.allParticipants })
+      explanation: t('profilePage.allParExpl', { count: stats?.allParticipants })
     },
     {
       value: stats?.ratingCount,
       label: t('profilePage.ratingCountLabel', { count: stats?.ratingCount }),
-      explanation: t('profilePage.ratingCountExpl', { data: stats?.ratingCount })
+      explanation: t('profilePage.ratingCountExpl', { count: stats?.ratingCount })
     },
     {
       value: stats?.averageRating?.toFixed(2) || '-',
@@ -31,12 +31,12 @@ export const UserStatCard = ({ stats }: { stats?: UserStats }) => {
     {
       value: stats?.participationCount,
       label: t('profilePage.parCountLabel', { count: stats?.participationCount }),
-      explanation: t('profilePage.parCountExpl', { data: stats?.participationCount })
+      explanation: t('profilePage.parCountExpl', { count: stats?.participationCount })
     },
     {
       value: stats?.requestCount,
       label: t('profilePage.reqCountLabel', { count: stats?.requestCount }),
-      explanation: t('profilePage.reqCountExpl', { data: stats?.requestCount })
+      explanation: t('profilePage.reqCountExpl', { count: stats?.requestCount })
     }
   ]
   return (
