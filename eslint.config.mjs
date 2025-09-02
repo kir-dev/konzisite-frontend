ddaca133fdfdf52fa4cc633daa750287a337eef9
Sámuel Fekete <feketesamu@gmail.com>
2025-09-02 16:25:57 +0200

    iounno man

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,50 +9,30 @@ import { fileURLToPath } from 'node:url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-})
+const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: js.configs.recommended, allConfig: js.configs.all })
 
 export default defineConfig([
   globalIgnores(['**/node_modules', '**/dist', '.yarn/*']),
   {
     extends: compat.extends('prettier', 'plugin:prettier/recommended'),
 
-    plugins: {
-      'react-hooks': fixupPluginRules(reactHooks)
-    },
+    plugins: { 'react-hooks': fixupPluginRules(reactHooks) },
 
     languageOptions: {
       parser: tsParser,
       ecmaVersion: 2018,
       sourceType: 'module',
 
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true
-        }
-      }
+      parserOptions: { ecmaFeatures: { jsx: true } }
     },
 
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    },
+    settings: { react: { version: 'detect' } },
 
     rules: {
       'react/jsx-props-no-spreading': 'off',
       'react/require-default-props': 'off',
 
-      'max-len': [
-        'error',
-        {
-          code: 140,
-          ignoreUrls: true
-        }
-      ],
+      'max-len': ['error', { code: 140, ignoreUrls: true }],
 
       'import/prefer-default-export': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off'
