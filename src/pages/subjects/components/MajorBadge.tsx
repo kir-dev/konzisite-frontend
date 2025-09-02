d4fb9162bea894ac12d69ee8b9b24b40318c6c7d
Sámuel Fekete <feketesamu@gmail.com>
2025-09-02 15:19:48 +0200

    migrate chakra stackitem

--- src/pages/subjects/components/MajorBadge.tsx
+++ src/pages/subjects/components/MajorBadge.tsx
@@ -1,4 +1,4 @@
-import { Badge, StackItem } from '@chakra-ui/react'
+import { Badge, Box } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 import { Major } from '../../../api/model/subject.model'
 import { majorColor, majorVariant } from '../../../util/majorHelpers'
@@ -6,10 +6,10 @@ import { majorColor, majorVariant } from '../../../util/majorHelpers'
 export const MajorBadge = ({ major }: { major: Major }) => {
   const { t } = useTranslation()
   return (
-    <StackItem>
+    <Box flexShrink={0}>
       <Badge ml={0} mr={2} mb={1} colorScheme={majorColor[major]} variant={majorVariant[major]}>
         {t(major)}
       </Badge>
-    </StackItem>
+    </Box>
   )
 }
