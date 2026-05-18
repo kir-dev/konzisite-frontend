590228e4fc771cc239e378a695e7b93b5d92342a
Isti01 <Horvatska01@gmail.com>
2026-05-18 19:19:12 +0200

    Update dependencies (#693)
    * Use same package manager
    
    * Update dependencies

--- src/components/commons/Markdown.tsx
+++ src/components/commons/Markdown.tsx
@@ -1,23 +1,146 @@
-import { Link } from '@chakra-ui/react'
-import ChakraUIRenderer from 'chakra-ui-markdown-renderer'
-import { ReactNode } from 'react'
-import ReactMarkdown from 'react-markdown'
-import remarkGemoji from 'remark-gemoji'
+import {
+  Code,
+  Divider,
+  Heading,
+  Image,
+  Link,
+  ListItem,
+  OrderedList,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  UnorderedList
+} from '@chakra-ui/react'
+import Markdown, { Components } from 'react-markdown'
+import { chakra } from '@chakra-ui/system'
 import remarkGfm from 'remark-gfm'
+import remarkGemoji from 'remark-gemoji'
 
-const theme: any = {
-  a: (props: { href: string; children: ReactNode }) => {
+type GetCoreProps = {
+  children?: React.ReactNode
+  'data-sourcepos'?: any
+}
+
+function getCoreProps(props: GetCoreProps): any {
+  return props['data-sourcepos'] ? { 'data-sourcepos': props['data-sourcepos'] } : {}
+}
+
+export const defaults: Components = {
+  p: (props) => {
+    const { children } = props
+    return <Text mb={2}>{children}</Text>
+  },
+  em: (props) => {
+    const { children } = props
+    return <Text as="em">{children}</Text>
+  },
+  blockquote: (props) => {
+    const { children } = props
+    return (
+      <Code as="blockquote" p={2}>
+        {children}
+      </Code>
+    )
+  },
+  code: (props) => {
+    const { children, className } = props
+
+    return <Code className={className} whiteSpace="break-spaces" display="block" w="full" p={2} children={children} />
+  },
+  del: (props) => {
+    const { children } = props
+    return <Text as="del">{children}</Text>
+  },
+  hr: (props) => {
+    return <Divider />
+  },
+  a: (props) => {
     const { href, children } = props
     return (
       <Link textColor="brand.200" textDecoration="underline" isExternal href={href}>
         {children}
       </Link>
     )
-  }
+  },
+  img: Image,
+  text: (props) => {
+    const { children } = props
+    return <Text as="span">{children}</Text>
+  },
+  ul: (props) => {
+    const { children } = props
+    const attrs = getCoreProps(props)
+    return (
+      <UnorderedList spacing={2} as={'ul'} styleType={'disc'} pl={4} {...attrs}>
+        {children}
+      </UnorderedList>
+    )
+  },
+  ol: (props) => {
+    const { children } = props
+    const attrs = getCoreProps(props)
+    return (
+      <OrderedList spacing={2} as={'ol'} styleType={'decimal'} pl={4} {...attrs}>
+        {children}
+      </OrderedList>
+    )
+  },
+  li: (props) => {
+    const { children } = props
+
+    return (
+      <ListItem {...getCoreProps(props)} listStyleType={'inherit'}>
+        {children}
+      </ListItem>
+    )
+  },
+  h1: (props) => (
+    <Heading my={4} as="h1" size="2xl" {...getCoreProps(props)}>
+      {props.children}
+    </Heading>
+  ),
+
+  h2: (props) => (
+    <Heading my={4} as="h2" size="xl" {...getCoreProps(props)}>
+      {props.children}
+    </Heading>
+  ),
+  h3: (props) => (
+    <Heading my={4} as="h3" size="lg" {...getCoreProps(props)}>
+      {props.children}
+    </Heading>
+  ),
+  h4: (props) => (
+    <Heading my={4} as="h4" size="md" {...getCoreProps(props)}>
+      {props.children}
+    </Heading>
+  ),
+  h5: (props) => (
+    <Heading my={4} as="h5" size="sm" {...getCoreProps(props)}>
+      {props.children}
+    </Heading>
+  ),
+  h6: (props) => (
+    <Heading my={4} as="h6" size="xs" {...getCoreProps(props)}>
+      {props.children}
+    </Heading>
+  ),
+  pre: (props) => {
+    const { children } = props
+    return <chakra.pre {...getCoreProps(props)}>{children}</chakra.pre>
+  },
+  table: Table,
+  thead: Thead,
+  tbody: Tbody,
+  tr: (props) => <Tr>{props.children}</Tr>,
+  td: (props) => <Td>{props.children}</Td>,
+  th: (props) => <Th>{props.children}</Th>
 }
 
-const Markdown = ({ markdown }: { markdown?: string }) => {
-  return <ReactMarkdown components={ChakraUIRenderer(theme)} children={markdown || ''} remarkPlugins={[remarkGfm, remarkGemoji]} />
+export default ({ markdown }: { markdown?: string }) => {
+  return <Markdown components={defaults} children={markdown} remarkPlugins={[remarkGfm, remarkGemoji]} />
 }
-
-export default Markdown
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import Plausible from 'plausible-tracker'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
@@ -9,18 +8,11 @@ import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
 import { AuthProvider } from './api/contexts/auth/AuthContext'
 import customTheme from './assets/theme'
-import { FRONTEND_HOST } from './util/environment'
 import { initAxios, queryClient } from './util/query-client'
 
 initAxios()
 
 const root = createRoot(document.getElementById('root')!)
-
-const { enableAutoPageviews } = Plausible({
-  domain: FRONTEND_HOST,
-  apiHost: 'https://visit.kir-dev.hu'
-})
-enableAutoPageviews()
 
 root.render(
   <React.StrictMode>
